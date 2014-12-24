4046fa20a0798448453eb6d2a7189d111be36dd4
Scott J. Krieder <skrieder@iit.edu>
2014-12-24 18:13:43 +0000

    Adding simple bootstrap website to git repo.

---
+++ assets/js/get-date.js
@@ -0,0 +1,18 @@
+/*!
+ * IE10 viewport hack for Surface/desktop Windows 8 bug
+ * Copyright 2014 Twitter, Inc.
+ * Licensed under the Creative Commons Attribution 3.0 Unported License. For
+ * details, see http://creativecommons.org/licenses/by/3.0/.
+ */
+
+// See the Getting Started docs for more information:
+// http://getbootstrap.com/getting-started/#support-ie10-width
+
+(function myFunction() {
+    var d = new Date();
+    var n = d.getFullYear();
+
+    var string = "&copy; Scott Krieder ";
+
+    document.getElementById("demo").innerHTML = string + n;
+})();
